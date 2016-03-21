f325410469210b21dd37e06b7ab9814b7838c28b
R3 <s.cardoso@threeangle.ch>
2016-03-21 13:41:16 +0100

    Added comments and features to the temperature script

--- py/temperature.sh
+++ py/temperature.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
-rm temperature.txt
-sudo ./AdafruitDHT.py 22 22 > temperature.txt
+#
+# KEVIN AMADO
+# Script créant un fichier avec les données
+# de température de la sonde
+#
+# SAMUEL CARDOSO
+# Optimisation du script
+# Ajout de l'envoi du fichier vers un FTP
+#
+# CFPT-EI - 2016
+#
 
+HOST="10.134.96.205"
+USER='camera'
+PASSWD='camera'
+FILE='temperature.txt'
+PATH='/tmp/'
+
+#/KA/ Suppresion du fichier
+#/SC/ Suppression du fichier si celui-ci existe
+if [ -e $FILE ]
+then
+	rm $FILE
+fi
+
+#/KA/ Récupération des données de température de la sonde
+# et création d'un fichier contenant celle-ci
+sudo ./AdafruitDHT.py 22 22 > $FILE
+
+#/SC/ Envoie le fichier définit dans la variable FILE à l'hôte définit
+# dans la variable HOST dans le répertoire définit dans la variable PATH 
+scp $FILE $USER@$HOST:$PATH
 
