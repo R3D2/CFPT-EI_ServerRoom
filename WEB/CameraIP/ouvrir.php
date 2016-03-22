8242a7f94045e73d64e381bbf4b4823f69dbc53b
DylanWacker <Dylan.wckr@eduge.ch>
2016-03-22 18:39:26 +0100

    Update ouvrir.php

--- WEB/CameraIP/ouvrir.php
+++ WEB/CameraIP/ouvrir.php
@@ -1,5 +1,4 @@
 <?php
-
 // Connexion FTP
 $FTP_IP = "10.134.96.205";
 $FTP_Pseudo = "camera";
@@ -11,7 +10,8 @@ if (($ftp = ftp_connect($FTP_IP, 21)) == false) {
 }
 
 // Ne pas afficher les erreurs au visiteurs
-ini_set("display_errors",0);error_reporting(0);
+ini_set("display_errors", 0);
+error_reporting(0);
 
 // Connexion utilisateur
 if (!ftp_login($ftp, $FTP_Pseudo, $FTP_MotDePasse)) {
@@ -22,8 +22,8 @@ if (!ftp_login($ftp, $FTP_Pseudo, $FTP_MotDePasse)) {
 // récupère tout les fichier et les mets dans un tableau
 $listes_fichiers = ftp_nlist($ftp, "."); // Le point signifie le dossier actuel
 $NomFichier = $_GET['open'];
-$regexExtensionFichier= '^.*\.(jpg|jpeg|png|gif)$';
-$titre = eregi($regexExtensionFichier,$NomFichier,$regs);
+$regexExtensionFichier = '^.*\.(jpg|jpeg|png|gif)$';
+$titre = eregi($regexExtensionFichier, $NomFichier, $regs);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -46,15 +46,15 @@ $titre = eregi($regexExtensionFichier,$NomFichier,$regs);
 
     <center>
         <h1><div class="label label-info">Supervision server room</div></h1><br/><br/>    
-        <h3><div class="label label-success">Fichier ouvert <?php echo $_GET['open'];?></div></h3>    
-        <?php if($regs[1]=="jpg"){ ?>
-        <a><img src= "ftp://camera:camera@10.134.96.205/<?php echo $_GET['open']; ?>" border="0" height="300" width="225"></a> </br></br>
+        <h3><div class="label label-success">Fichier ouvert <?php echo $_GET['open']; ?></div></h3>    
+        <?php if ($regs[1] == "jpg") { ?>
+            <a><img src= "ftp://camera:camera@10.134.96.205/<?php echo $_GET['open']; ?>" border="0" height="300" width="225"></a> </br></br>
         <?php } else { ?>
-        <a><video controls src="ftp://camera:camera@10.134.96.205/<?php echo $_GET['open'];?>"  >Vidéo non supporté</video></a> </br></br>
+            <a><video controls src="ftp://camera:camera@10.134.96.205/<?php echo $_GET['open']; ?>"  type="video/avi">Vidéo non supporté</video></a> </br></br>
         <?php } ?>
         <a type="submit" class="btn btn-danger btn-link"  href="index.php" name="BoutonRetour" ><- Retour à l'index</a><br/>
     </center>
-      
+
     <?php
     ftp_close($ftp); // ferme connexion
     ?> 
